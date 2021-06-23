65b7fb38c505e259aebbbe0b3639a56cb36fcdca
Mustafa Adnan <Mustafa1993tofi@gmail.com>
2021-06-23 15:19:41 +0300

    Initial commit

---
+++ car_manager.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.goto()
+
+    def car_movement(self):
+        new_y = random.randint(-1, 100)
+        self.goto(300,)
