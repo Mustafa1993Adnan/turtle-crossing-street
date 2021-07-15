2990f3b3c261c67625f3ee61788efdfb83906502
Mustafa Adnan <Mustafa1993tofi@gmail.com>
2021-07-15 15:00:14 +0300

    initial commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 screen.listen()
 screen.onkeypress(player.move, "Up")
 
@@ -16,3 +17,14 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_car()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 26:
+            game_is_on = False
+
+    if player.is_turtle_at_finish_line():
+        player.go_start_position()
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
@@ -8,11 +9,19 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
-        self.color("green")
+        self.color("black")
         self.penup()
-        self.goto(STARTING_POSITION)
-
+        self.go_start_position()
         self.left(90)
+
+    def go_start_position(self):
+        self.goto(STARTING_POSITION)
 
     def move(self):
         self.forward(MOVE_DISTANCE)
+
+    def is_turtle_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
